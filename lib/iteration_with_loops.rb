b20cce0ea348a954fd6bb8a359a6d75428339210
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-08 16:52:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,10 @@
 def find_even_values(src)
-  new_array=[]
   counter=0 
   while counter<src.length do 
   element_index=0 
   while element_index<src[counter].length do 
     if src[counter][element_index].even?   
-      new_array << src[counter][element_index]
+      p src[counter][element_index]
     end
     element_index+=1 
   end 
